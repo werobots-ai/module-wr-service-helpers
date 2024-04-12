48edc9229d41a906ba28d150f17afeb9fb0ea4b8
Norbert Nemeth <nnorb93@gmail.com>
2024-04-12 15:24:34 +0200

    feat: add validation middleware

---
+++ src/middleware/validation.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodObject } from "zod";
+
+export default (schema: ZodObject<any>, property: "body" | "query" | "params" = "body") =>
+  (req: Request, _: Response, next: NextFunction) => {
+    const result = schema.safeParse(req[property]);
+    if (result.success) {
+      req[property] = result.data;
+      return next();
+    }
+
+    const {
+      error: { issues },
+    } = result;
+
+    switch (issues[0].message) {
+      case "Required":
+        return next({
+          status: 422,
+          message: "Validation error",
+          field: issues[0]?.path[0] || "unknown field",
+        });
+      default:
+        return next({
+          status: 422,
+          message: "Validation error",
+          issues,
+        });
+    }
+  };
